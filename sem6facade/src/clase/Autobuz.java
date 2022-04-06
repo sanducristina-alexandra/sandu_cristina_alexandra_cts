02e7e39ad85cce22fda9efee83da5e643b331f38
sanducristina-alexandra <sanducristina19@stud.ase.ro>
2022-04-06 23:16:05 +0300

    added the facade classes

---
+++ sem6facade/src/clase/Autobuz.java
@@ -0,0 +1,22 @@
+package clase;
+
+public class Autobuz {
+    public void deschideUsaFata(){
+        System.out.println("Se deschide usa din fata");
+    }
+    public void deschideUsaMijloc(){
+        System.out.println("Se deschide usa din mijloc");
+    }
+    public void deschideUsaSpate(){
+        System.out.println("Se deschide usa din spate");
+    }
+    public void liberUsaFata(){
+        System.out.println("Usa fata liber");
+    }
+    public void liberUsaMijloc(){
+        System.out.println("Usa mijloc liber");
+    }
+    public void liberUsaSpate(){
+        System.out.println("Usa spate liber");
+    }
+}

fcfa1a627dda9b6492ce7fbdd5fffe214c62712f
sanducristina-alexandra <sanducristina19@stud.ase.ro>
2022-04-27 23:27:41 +0300

    added the decorator classes

---
+++ sem7decorator/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import clase.Decorator;
+import clase.DecoratorOcazional;
+import clase.Printer;
+import clase.PrinterBilet;
+
+public class Main {
+    public static void main(String[] args) {
+        Printer printer = new PrinterBilet();
+        Decorator decorator = new DecoratorOcazional(printer,"La multi ani!");
+        printer.printeazaBilet();
+        decorator.printeazaMesaj();
+    }
+}

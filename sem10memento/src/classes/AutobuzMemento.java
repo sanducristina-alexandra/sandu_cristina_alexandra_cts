71fe2fc5e7222c624fcb2870e180f192c57c9ca1
sanducristina-alexandra <sanducristina19@stud.ase.ro>
2022-05-19 23:10:23 +0300

    added memento classes

---
+++ sem10memento/src/classes/AutobuzMemento.java
@@ -0,0 +1,19 @@
+package classes;
+
+public class AutobuzMemento {
+    private String nume;
+    private  Integer consum;
+
+    public AutobuzMemento(String nume, Integer consum) {
+        this.nume = nume;
+        this.consum = consum;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public Integer getConsum() {
+        return consum;
+    }
+}
